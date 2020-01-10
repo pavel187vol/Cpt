7a706111b9d53ee75cc9903703658567c76e9e4c
pavel187vol <voloscovich.pavel@gmail.com>
2020-01-10 16:32:45 +0300

    secret key heroku

--- crp/settings/settings_prodaction.py
+++ crp/settings/settings_prodaction.py
@@ -1,9 +1,4 @@
 from .base import *
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-DEBUG = False
 
 ADMINS = (
     ('Pavel V', 'pavel.voloscovich@gmail.com'),
@@ -22,3 +17,10 @@ DATABASES = {
        'PORT' : '5432',
    }
 }
+
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DEBUG = False
