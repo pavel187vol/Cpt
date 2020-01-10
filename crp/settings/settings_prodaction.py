272b55ca3fa850fb015cec3556d668c0484161e0
pavel187vol <voloscovich.pavel@gmail.com>
2020-01-10 20:07:14 +0300

    Error 500

--- crp/settings/settings_prodaction.py
+++ crp/settings/settings_prodaction.py
@@ -7,16 +7,16 @@ ADMINS = (
 
 ALLOWED_HOSTS = ['frozen-mesa-66627.herokuapp.com']
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'crp',
-       'USER': 'rood',
-       'PASSWORD': 'zhelezo',
-       'HOST' : '127.0.0.1',
-       'PORT' : '5432',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'crp',
+#        'USER': 'rood',
+#        'PASSWORD': 'zhelezo',
+#        'HOST' : '127.0.0.1',
+#        'PORT' : '5432',
+#    }
+# }
 
 
 import dj_database_url
